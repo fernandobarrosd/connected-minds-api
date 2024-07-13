b00f63b3272d05580a43fc9431b17ef147834d4f
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-13 16:46:12 -0300

    Create AuthController

---
+++ src/main/java/com/fernando/connected_minds_api/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.fernando.connected_minds_api.controllers;
+
+import com.fernando.connected_minds_api.requests.LoginRequest;
+import com.fernando.connected_minds_api.requests.RefreshTokenRequest;
+import com.fernando.connected_minds_api.requests.RegisterRequest;
+import com.fernando.connected_minds_api.responses.AuthResponse;
+import com.fernando.connected_minds_api.services.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody @Valid LoginRequest login) {
+        return ResponseEntity.ok(authService.authenticate(login));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> registerUser(@RequestBody @Valid RegisterRequest registerRequest) {
+        return ResponseEntity.created(null).body(authService.registerUser(registerRequest));
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<AuthResponse> generateNewToken(@RequestBody @Valid RefreshTokenRequest request) {
+        return ResponseEntity.ok(authService.generateNewToken(request));
+    }
+}
