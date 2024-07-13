7bacbb1fbdab7753e0bca15ba8cc3b24000450d4
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-13 16:56:43 -0300

    Update UserGenreConverter convertToEntityAttribute method

--- src/main/java/com/fernando/connected_minds_api/enums/converters/UserGenreConverter.java
+++ src/main/java/com/fernando/connected_minds_api/enums/converters/UserGenreConverter.java
@@ -4,6 +4,9 @@ import com.fernando.connected_minds_api.enums.UserGenre;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Converter(autoApply = true)
 public class UserGenreConverter implements AttributeConverter<UserGenre, String> {
 
@@ -14,6 +17,14 @@ public class UserGenreConverter implements AttributeConverter<UserGenre, String>
 
     @Override
     public UserGenre convertToEntityAttribute(String genreValue) {
-        return UserGenre.valueOf(genreValue);
+        List<UserGenre> userGenre = Arrays.stream(UserGenre.values())
+                .filter(genre -> genre.getValue().equals(genreValue))
+                .toList();
+
+        if (userGenre.isEmpty()) {
+            throw new RuntimeException("Enum conversion error");
+        }
+        return userGenre.getFirst();
+
     }
 }
