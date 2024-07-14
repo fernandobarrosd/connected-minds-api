550effaa4a0c9b074f67f7ec98db1c518ac93858
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-14 13:12:05 -0300

    Create UserResponse

---
+++ src/main/java/com/fernando/connected_minds_api/responses/UserResponse.java
@@ -0,0 +1,28 @@
+package com.fernando.connected_minds_api.responses;
+
+import com.fernando.connected_minds_api.enums.UserStatus;
+import com.fernando.connected_minds_api.models.User;
+import java.util.UUID;
+
+public record UserResponse(
+        UUID id,
+        String username,
+        String genre,
+        Boolean isOnline,
+        String bio,
+        String photoURL,
+        String bannerURL) {
+    public static UserResponse fromEntity(User user) {
+        Boolean isOnline = user.getStatus() == UserStatus.ONLINE;
+
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getGenre().getValue(),
+                isOnline,
+                user.getBio(),
+                user.getPhotoURL(),
+                user.getBannerURL()
+        );
+    }
+}
