e536c782c53240368740eb49c994f592709d1df9
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-14 16:25:13 -0300

    Update SecurityConfig

--- src/main/java/com/fernando/connected_minds_api/config/SecurityConfig.java
+++ src/main/java/com/fernando/connected_minds_api/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.fernando.connected_minds_api.config;
 
+import com.fernando.connected_minds_api.filters.JWTFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,12 +14,15 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JWTFilter jwtFilter;
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -34,9 +38,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/h2-console", "/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/h2-console", "/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/users/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .headers(headers -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
