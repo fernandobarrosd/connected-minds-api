e01987c2d17a8392fcd8aecc9a8014144cadea7f
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-16 18:47:04 -0300

    Add validation annotations in PostRequest

--- src/main/java/com/fernando/connected_minds_api/requests/PostRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/PostRequest.java
@@ -1,3 +1,11 @@
 package com.fernando.connected_minds_api.requests;
 
-public record PostRequest(String content, String photoURL) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PostRequest(
+        @NotNull(message = "content is required")
+        @NotBlank(message = "content not should be empty")
+        String content,
+
+        String photoURL) {}
