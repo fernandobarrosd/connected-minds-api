9b4b5b8723c5f949d0f97b1c4fdcc7bd8361e447
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-16 18:16:16 -0300

    Create Tag model

---
+++ src/main/java/com/fernando/connected_minds_api/models/Tag.java
@@ -0,0 +1,24 @@
+package com.fernando.connected_minds_api.models;
+
+import com.fernando.connected_minds_api.models.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tag_model")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Tag extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    public Tag(String name) {
+        super(null);
+        this.name = name;
+    }
+}
