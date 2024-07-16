1bc130e62b4390218bdde273eb101da9ab80dc7b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-16 18:16:40 -0300

    Create TagRepository

---
+++ src/main/java/com/fernando/connected_minds_api/repositories/TagRepository.java
@@ -0,0 +1,8 @@
+package com.fernando.connected_minds_api.repositories;
+
+import com.fernando.connected_minds_api.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+}
