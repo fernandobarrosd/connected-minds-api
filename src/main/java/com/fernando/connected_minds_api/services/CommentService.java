36a5c0b2987a55a211b79ed9f51a58b311337b65
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-22 17:29:06 -0300

    Create findCommentById method in CommentService

--- src/main/java/com/fernando/connected_minds_api/services/CommentService.java
+++ src/main/java/com/fernando/connected_minds_api/services/CommentService.java
@@ -1,10 +1,11 @@
 package com.fernando.connected_minds_api.services;
 
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
+import com.fernando.connected_minds_api.models.Comment;
 import com.fernando.connected_minds_api.repositories.CommentRepository;
+import com.fernando.connected_minds_api.responses.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
@@ -14,8 +15,15 @@ public class CommentService {
 
     public void deleteComment(UUID commentID) {
         if (!commentRepository.existsById(commentID)) {
-            throw new EntityNotFoundException("Post is not exists");
+            throw new EntityNotFoundException("Comment is not exists");
         }
         commentRepository.deleteById(commentID);
+    }
+
+    public CommentResponse findCommentById(UUID commentID) {
+        Comment comment = commentRepository.findById(commentID)
+                .orElseThrow(() -> new EntityNotFoundException("Comment is not exists"));
+
+        return CommentResponse.fromEntity(comment);
     }
 }
