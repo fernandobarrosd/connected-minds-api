ab0bfcdfe6a2517f07cf195a342877545a188de3
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-22 19:13:01 -0300

    Create findAllCommentsOfComment method in CommentRepository

--- src/main/java/com/fernando/connected_minds_api/repositories/CommentRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/CommentRepository.java
@@ -3,9 +3,13 @@ package com.fernando.connected_minds_api.repositories;
 import com.fernando.connected_minds_api.models.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
     List<Comment> findAllByPostId(UUID postId, Pageable pageable);
+
+    @Query("SELECT c.comments FROM Comment c WHERE c.id = :commentID")
+    List<Comment> findAllCommentsOfComment(UUID commentID, Pageable pageable);
 }
