74b1802998fd51e4227979d221c9af23622d9206
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-22 17:27:52 -0300

    Create findCommentById method in CommentController

--- src/main/java/com/fernando/connected_minds_api/controllers/CommentController.java
+++ src/main/java/com/fernando/connected_minds_api/controllers/CommentController.java
@@ -1,9 +1,12 @@
 package com.fernando.connected_minds_api.controllers;
 
+import com.fernando.connected_minds_api.responses.CommentResponse;
 import com.fernando.connected_minds_api.services.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,5 +23,10 @@ public class CommentController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteComment(@PathVariable UUID commentID) {
         commentService.deleteComment(commentID);
+    }
+
+    @GetMapping("/{commentID}")
+    public ResponseEntity<CommentResponse> findCommentById(@PathVariable UUID commentID) {
+        return ResponseEntity.ok(commentService.findCommentById(commentID));
     }
 }
