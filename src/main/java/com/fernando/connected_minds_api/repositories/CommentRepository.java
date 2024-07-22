b7cec057ea39c348bda4dc6fe3221cfb71e80393
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-22 17:26:00 -0300

    Create findAllByPostId method in CommentRepository

--- src/main/java/com/fernando/connected_minds_api/repositories/CommentRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.fernando.connected_minds_api.repositories;
 
 import com.fernando.connected_minds_api.models.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findAllByPostId(UUID postId, Pageable pageable);
 }
