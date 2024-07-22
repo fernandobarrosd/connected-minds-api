0975cb8b1cae4b7a918609d19f4d1f2295890470
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-22 15:24:45 -0300

    Create fromEntity static method in CommentResponse

--- src/main/java/com/fernando/connected_minds_api/responses/CommentResponse.java
+++ src/main/java/com/fernando/connected_minds_api/responses/CommentResponse.java
@@ -1,5 +1,6 @@
 package com.fernando.connected_minds_api.responses;
 
+import com.fernando.connected_minds_api.models.Comment;
 import lombok.Builder;
 
 import java.util.UUID;
@@ -11,4 +12,15 @@ public record CommentResponse(
         Long likes,
         String createdAt,
         UUID ownerID,
-        UUID postID) {}
+        UUID postID) {
+    public static CommentResponse fromEntity(Comment comment) {
+        return CommentResponse.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .likes(comment.getLikes())
+                .createdAt(comment.getCreatedAt().toString())
+                .ownerID(comment.getOwner().getId())
+                .postID(comment.getPost().getId())
+                .build();
+    }
+}
