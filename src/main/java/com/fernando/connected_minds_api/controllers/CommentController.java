39ccc086fc90a07fb2415a5441583b49b13162d2
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-23 12:14:53 -0300

    Create createCommentOfComment and findAllCommentsOfComment CommentController methods

--- src/main/java/com/fernando/connected_minds_api/controllers/CommentController.java
+++ src/main/java/com/fernando/connected_minds_api/controllers/CommentController.java
@@ -1,16 +1,15 @@
 package com.fernando.connected_minds_api.controllers;
 
+import com.fernando.connected_minds_api.models.User;
+import com.fernando.connected_minds_api.requests.CommentRequest;
 import com.fernando.connected_minds_api.responses.CommentResponse;
 import com.fernando.connected_minds_api.services.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,5 +27,22 @@ public class CommentController {
     @GetMapping("/{commentID}")
     public ResponseEntity<CommentResponse> findCommentById(@PathVariable UUID commentID) {
         return ResponseEntity.ok(commentService.findCommentById(commentID));
+    }
+
+    @PostMapping("/{commentID}/comments")
+    public ResponseEntity<CommentResponse> createCommentOfComment(
+            @AuthenticationPrincipal User owner,
+            @PathVariable UUID commentID,
+            @RequestBody CommentRequest commentRequest) {
+
+        return ResponseEntity.created(null).body(commentService.createCommentOfComment(commentID, commentRequest, owner));
+    }
+
+    @GetMapping("/{commentID}/comments")
+    public ResponseEntity<List<CommentResponse>> findAllCommentsOfComment(
+            @PathVariable UUID commentID,
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "5") Integer itemsPerPage) {
+        return ResponseEntity.ok(commentService.findAllCommentsOfComment(commentID, page, itemsPerPage));
     }
 }
