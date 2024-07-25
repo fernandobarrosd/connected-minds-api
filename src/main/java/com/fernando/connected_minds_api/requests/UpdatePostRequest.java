74551a1572d5cf020d5004dfeca14ff135c4ee73
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-25 17:19:37 -0300

    Add likes attribute in UpdatePostRequest record

--- src/main/java/com/fernando/connected_minds_api/requests/UpdatePostRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/UpdatePostRequest.java
@@ -1,12 +1,14 @@
 package com.fernando.connected_minds_api.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record UpdatePostRequest(
         @NotBlank(message = "content not should be empty")
         String content,
 
         @NotBlank(message = "photoURL not should be empty")
-        String photoURL
-) {
-}
+        String photoURL,
+
+        @PositiveOrZero(message = "likes should be positive or zero")
+        Long likes) {}
