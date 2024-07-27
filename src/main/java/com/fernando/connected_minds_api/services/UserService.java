9300c16716c88e2145291c94aa45f781fd3237ae
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-27 15:48:48 -0300

    Create findAllUserCommunities method in UserService

--- src/main/java/com/fernando/connected_minds_api/services/UserService.java
+++ src/main/java/com/fernando/connected_minds_api/services/UserService.java
@@ -3,9 +3,14 @@ package com.fernando.connected_minds_api.services;
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
 import com.fernando.connected_minds_api.models.User;
 import com.fernando.connected_minds_api.repositories.UserRepository;
+import com.fernando.connected_minds_api.responses.CommunityResponse;
 import com.fernando.connected_minds_api.responses.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +23,13 @@ public class UserService {
                 .orElseThrow(() -> new EntityNotFoundException("%s is not exists".formatted(username)));
 
         return UserResponse.toResponse(user);
+    }
+
+    public List<CommunityResponse> findAllUserCommunities(UUID userID, Integer page, Integer itemsPerPage) {
+        Pageable pageable = PageRequest.of(page, itemsPerPage);
+        return userRepository.findAllCommunities(userID, pageable)
+                .stream()
+                .map(CommunityResponse::toResponse)
+                .toList();
     }
 }
