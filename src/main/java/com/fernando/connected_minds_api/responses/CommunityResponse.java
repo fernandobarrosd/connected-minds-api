73b330a2d3b9b484da16000be4821983edb1f7c9
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-27 15:41:34 -0300

    Create toResponse overload receiving only the Community model and remove ownerID field in CommunityResponse

--- src/main/java/com/fernando/connected_minds_api/responses/CommunityResponse.java
+++ src/main/java/com/fernando/connected_minds_api/responses/CommunityResponse.java
@@ -1,6 +1,7 @@
 package com.fernando.connected_minds_api.responses;
 
 import com.fernando.connected_minds_api.models.Community;
+import com.fernando.connected_minds_api.models.Tag;
 import com.fernando.connected_minds_api.models.User;
 import lombok.Builder;
 import java.util.List;
@@ -18,9 +19,8 @@ public record CommunityResponse(
         Long groupsCount,
         Long postCount,
         Long adminsCount,
-        UUID ownerID,
         List<String> tags) {
-    public static CommunityResponse toResponse(Community community, User owner, List<String> tags) {
+    public static CommunityResponse toResponse(Community community, List<String> tags) {
         return CommunityResponse.builder()
                 .id(community.getId())
                 .name(community.getName())
@@ -32,8 +32,23 @@ public record CommunityResponse(
                 .groupsCount((long) community.getGroups().size())
                 .postCount((long) community.getPosts().size())
                 .adminsCount((long) community.getAdmins().size())
-                .ownerID(owner.getId())
                 .tags(tags)
+                .build();
+    }
+
+    public static CommunityResponse toResponse(Community community) {
+        return CommunityResponse.builder()
+                .id(community.getId())
+                .name(community.getName())
+                .description(community.getDescription())
+                .photoURL(community.getPhotoURL())
+                .bannerURL(community.getBannerURL())
+                .createdAt(community.getCreatedAt().toString())
+                .membersCount((long) community.getMembers().size())
+                .groupsCount((long) community.getGroups().size())
+                .postCount((long) community.getPosts().size())
+                .adminsCount((long) community.getAdmins().size())
+                .tags(community.getTags().stream().map(Tag::getName).toList())
                 .build();
     }
 }
