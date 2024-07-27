cd76dfda354eb78cbea905a3d3d93485dc5e6998
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-27 15:48:33 -0300

    Create findAllUserCommunities method in UserController

--- src/main/java/com/fernando/connected_minds_api/controllers/UserController.java
+++ src/main/java/com/fernando/connected_minds_api/controllers/UserController.java
@@ -1,13 +1,14 @@
 package com.fernando.connected_minds_api.controllers;
 
+import com.fernando.connected_minds_api.models.User;
+import com.fernando.connected_minds_api.responses.CommunityResponse;
 import com.fernando.connected_minds_api.responses.UserResponse;
 import com.fernando.connected_minds_api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -18,5 +19,14 @@ public class UserController {
     @GetMapping("/{username}")
     public ResponseEntity<UserResponse> findUserByUsername(@PathVariable String username) {
         return ResponseEntity.ok(userService.findUserByUsername(username));
+    }
+
+    @GetMapping("/communities")
+    public ResponseEntity<List<CommunityResponse>> findAllUserCommunities(
+            @AuthenticationPrincipal User user,
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "5") Integer itemsPerPage) {
+        return ResponseEntity.ok(
+                userService.findAllUserCommunities(user.getId(), page, itemsPerPage));
     }
 }
