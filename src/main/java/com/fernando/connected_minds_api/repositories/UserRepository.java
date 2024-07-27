a71cafd8f8f568727a67ba678455c7d9e25c32ce
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-27 14:52:36 -0300

    Create findAllCommunities method in UserRepository

--- src/main/java/com/fernando/connected_minds_api/repositories/UserRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.fernando.connected_minds_api.repositories;
 
+import com.fernando.connected_minds_api.models.Community;
 import com.fernando.connected_minds_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    @Query("SELECT u.communities FROM User u WHERE u.id = :userID")
+    List<Community> findAllCommunities(UUID userID);
 }
