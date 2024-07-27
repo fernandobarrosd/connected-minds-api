db9c79585431dc5a593aa1ec59c0d8dc9253d6b7
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-27 15:49:57 -0300

    Add pageable of Pageable type parameter in findAllCommunities UserRepository method

--- src/main/java/com/fernando/connected_minds_api/repositories/UserRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.fernando.connected_minds_api.repositories;
 
 import com.fernando.connected_minds_api.models.Community;
 import com.fernando.connected_minds_api.models.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
@@ -12,5 +13,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
     @Query("SELECT u.communities FROM User u WHERE u.id = :userID")
-    List<Community> findAllCommunities(UUID userID);
+    List<Community> findAllCommunities(UUID userID, Pageable pageable);
 }
