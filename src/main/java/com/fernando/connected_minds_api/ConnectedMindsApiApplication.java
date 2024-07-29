dfd0d160742777cfc3a7d981fbad12774b143a7c
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-29 13:52:09 -0300

    Add EnableCaching annotation in ConnectedMindsApiApplication for activate cache in spring boot application

--- src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
+++ src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
@@ -2,12 +2,12 @@ package com.fernando.connected_minds_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ConnectedMindsApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConnectedMindsApiApplication.class, args);
     }
-
 }
