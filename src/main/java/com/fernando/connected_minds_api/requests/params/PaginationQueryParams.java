16b55119a7a1e6f88d80e70aa4dd8549914e78f0
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-30 14:24:24 -0300

    Create PaginationQueryParams

---
+++ src/main/java/com/fernando/connected_minds_api/requests/params/PaginationQueryParams.java
@@ -0,0 +1,15 @@
+package com.fernando.connected_minds_api.requests.params;
+
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PaginationQueryParams {
+    @PositiveOrZero(message = "The page should be positive number")
+    private Integer page = 0;
+
+    @PositiveOrZero(message = "The page should be positive number")
+    private  Integer itemsPerPage = 5;
+}
