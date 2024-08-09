33aa889cd28b018542aa67c72adfd1a4ca170077
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-09 13:24:02 -0300

    Define findAllByLocationID method on PostRepository interface

--- src/main/java/com/fernando/connected_minds_api/repositories/PostRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/PostRepository.java
@@ -2,8 +2,10 @@ package com.fernando.connected_minds_api.repositories;
 
 import com.fernando.connected_minds_api.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post> findAllByLocationID(UUID locationID);
 }
