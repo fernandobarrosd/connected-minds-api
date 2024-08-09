e9e352f5a7daf1a69dd5b700e17e16feb10050ab
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-08 22:02:30 -0300

    Define findAllByCommunityId on GroupRepository interface

--- src/main/java/com/fernando/connected_minds_api/repositories/GroupRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/GroupRepository.java
@@ -1,11 +1,11 @@
 package com.fernando.connected_minds_api.repositories;
 
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import com.fernando.connected_minds_api.models.Group;
 
 public interface GroupRepository extends JpaRepository<Group, UUID>{
-    
+    List<Group> findAllByCommunityId(UUID communityId, Pageable pageable);
 }
