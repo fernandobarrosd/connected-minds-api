b1415c9ca06f71f764d408afd63a27a0e0fc8370
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-09 14:43:47 -0300

    Create createGroup method on CommunityController

--- src/main/java/com/fernando/connected_minds_api/controllers/CommunityController.java
+++ src/main/java/com/fernando/connected_minds_api/controllers/CommunityController.java
@@ -2,16 +2,23 @@ package com.fernando.connected_minds_api.controllers;
 
 import com.fernando.connected_minds_api.models.User;
 import com.fernando.connected_minds_api.requests.CommunityRequest;
+import com.fernando.connected_minds_api.requests.GroupRequest;
 import com.fernando.connected_minds_api.responses.CommunityResponse;
+import com.fernando.connected_minds_api.responses.GroupResponse;
 import com.fernando.connected_minds_api.services.CommunityService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.fernando.connected_minds_api.requests.params.PaginationQueryParams;
 
 @RestController
 @RequestMapping("/communities")
@@ -25,5 +32,21 @@ public class CommunityController {
             @RequestBody @Valid CommunityRequest communityRequest) {
         return ResponseEntity.created(null).body(
                 communityService.createCommunity(communityRequest, owner));
+    }
+
+    @PostMapping("/{communityID}/groups")
+    public ResponseEntity<GroupResponse> createGroup(
+        @AuthenticationPrincipal User user,
+        @RequestBody @Valid GroupRequest groupRequest,
+        @PathVariable @Valid String communityID) {
+            return ResponseEntity.created(null).body(
+                communityService.createGroup(user, groupRequest, UUID.fromString(communityID)));
+
+        }
+    @GetMapping("/{communityID}/groups")
+    public ResponseEntity<List<GroupResponse>> findAllGroups(
+        @PathVariable @Valid String communityID,
+        @Valid PaginationQueryParams pagination) {
+        return ResponseEntity.ok(communityService.findAllGroups(UUID.fromString(communityID), pagination));
     }
 }
