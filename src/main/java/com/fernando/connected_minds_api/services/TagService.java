eeea05e5f1b45a2314b377282e4ae294cb31c23d
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-08 22:01:48 -0300

    Create saveAllTags method on TagService

--- src/main/java/com/fernando/connected_minds_api/services/TagService.java
+++ src/main/java/com/fernando/connected_minds_api/services/TagService.java
@@ -1,10 +1,13 @@
 package com.fernando.connected_minds_api.services;
 
+import com.fernando.connected_minds_api.models.Tag;
 import com.fernando.connected_minds_api.repositories.TagRepository;
+import com.fernando.connected_minds_api.requests.TagRequest;
 import com.fernando.connected_minds_api.responses.TagResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +19,21 @@ public class TagService {
                 .stream()
                 .map(TagResponse::toResponse)
                 .toList();
+    }
+
+    public List<Tag> saveAllTags(List<TagRequest> tagsRequest) {
+        List<Tag> tags = tagsRequest
+                .stream()
+                .map(tag -> {
+                        if (tag.id() == null) {
+                                System.out.println(tag.id());
+                                return new Tag(tag.name());
+                        }
+                        return new Tag(UUID.fromString(tag.id()), tag.name());
+                })
+                .toList();
+        
+        tagRepository.saveAll(tags);
+        return tags;
     }
 }
