5fb5d546e9edab17495e936f259d28bb77ac4b75
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-13 20:49:21 -0300

    Add validation annotations on CommunityRequest

--- src/main/java/com/fernando/connected_minds_api/requests/CommunityRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/CommunityRequest.java
@@ -1,10 +1,23 @@
 package com.fernando.connected_minds_api.requests;
 
 import java.util.List;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record CommunityRequest(
+        @NotNull(message = "name field is required")
+        @NotEmpty(message = "name field not should be empty")
         String name,
+
+        @NotNull(message = "description field is required")
+        @NotEmpty(message = "description field not should be empty")
         String description,
+
+        @NotNull(message = "tags field is required")
         List<TagRequest> tags,
+
+        @NotNull(message = "photoURL field is required")
         String photoURL,
+
+        @NotNull(message = "bannerURL field is required")
         String bannerURL) {}
