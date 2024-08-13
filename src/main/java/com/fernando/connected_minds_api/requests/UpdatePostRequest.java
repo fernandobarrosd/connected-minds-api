496e46a2a2d8aa03635fb5f0fa62b5219a2c62a8
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-13 20:44:56 -0300

    Update validation messages on UpdatePostRequest

--- src/main/java/com/fernando/connected_minds_api/requests/UpdatePostRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/UpdatePostRequest.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record UpdatePostRequest(
-        @NotBlank(message = "content not should be empty")
+        @NotBlank(message = "content field not should be empty")
         String content,
 
-        @NotBlank(message = "photoURL not should be empty")
+        @NotBlank(message = "photoURL field not should be empty")
         String photoURL,
 
-        @PositiveOrZero(message = "likes should be positive or zero")
+        @PositiveOrZero(message = "likes field should be positive or zero")
         Long likes) {}
