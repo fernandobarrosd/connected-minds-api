d4015134dafe08b47285c9bac27cb353c180230b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-13 20:50:10 -0300

    Add Pattern annotation for validate name field on TagRequest

--- src/main/java/com/fernando/connected_minds_api/requests/TagRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/TagRequest.java
@@ -2,10 +2,12 @@ package com.fernando.connected_minds_api.requests;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record TagRequest(
     String id,
 
-    @NotNull(message = "name is required")
-    @NotBlank(message = "name not should be empty")
+    @NotNull(message = "name field is required")
+    @NotBlank(message = "name field not should be empty")
+    @Pattern(regexp = "^[a-z][a-zA-Z0-9_]*", message = "tag name should be starts with lower case letter and can include this characters: [a-z], [A-Z], [0-9], _")
     String name) {}
