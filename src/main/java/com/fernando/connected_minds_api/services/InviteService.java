102a68b51942be6b5efde769785018cebe4431f5
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-23 13:46:09 -0300

    Remove CommunityRepository communityRepository and GroupRepository groupRepository attributes, update createInvite method and create acceptInvite method in the InviteService

--- src/main/java/com/fernando/connected_minds_api/services/InviteService.java
+++ src/main/java/com/fernando/connected_minds_api/services/InviteService.java
@@ -4,27 +4,31 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
 import com.fernando.connected_minds_api.models.Invite;
-import com.fernando.connected_minds_api.repositories.CommunityRepository;
-import com.fernando.connected_minds_api.repositories.GroupRepository;
 import com.fernando.connected_minds_api.repositories.InviteRepository;
 import com.fernando.connected_minds_api.responses.InviteResponse;
 import lombok.RequiredArgsConstructor;
+import com.fernando.connected_minds_api.models.User;
+import com.fernando.connected_minds_api.enums.InviteType;
 
 @Service
 @RequiredArgsConstructor
 public class InviteService {
     private final InviteRepository inviteRepository;
-    private final CommunityRepository communityRepository;
-    private final GroupRepository groupRepository;
+    private final CommunityService communityService;
+    private final GroupService groupService;
 
 
     public InviteResponse createInvite(UUID fromID) {
         Boolean fromIDIsNotExists = false;
+        InviteType inviteType = null;
 
-        if (!communityRepository.existsById(fromID)) {
-            fromIDIsNotExists = true;
+        if (communityService.communityIsExists(fromID)) {
+            inviteType = InviteType.COMMUNITY;
         }
-        else if (!groupRepository.existsById(fromID)) {
+        else if (groupService.groupIsExists(fromID)) {
+            inviteType = InviteType.GROUP;
+        }
+        else {
             fromIDIsNotExists = true;
         }
 
@@ -32,9 +36,24 @@ public class InviteService {
             throw new EntityNotFoundException("Community or group is not exists");
         }
 
-        Invite invite = new Invite(fromID);
+        Invite invite = new Invite(fromID, inviteType);
 
         inviteRepository.save(invite);
+
+        return InviteResponse.toResponse(invite);
+    }
+
+
+    public InviteResponse acceptInvite(User user, UUID inviteID) {
+        Invite invite = inviteRepository.findById(inviteID)
+        .orElseThrow(() -> new EntityNotFoundException("Invite is not exists"));
+
+        if (invite.getType() == InviteType.COMMUNITY) {
+            communityService.addMemberOnCommunity(invite.getFromID(), user);
+        }
+        else {
+           groupService.addMemberOnGroup(invite.getFromID(), user);
+        }
 
         return InviteResponse.toResponse(invite);
     }
