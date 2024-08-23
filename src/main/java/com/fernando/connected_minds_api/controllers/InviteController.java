51c0cb1d1c0a6c0ad46a6d79b1c7c5157d65815a
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-23 13:41:58 -0300

    Create acceptInvite method in the InviteController

--- src/main/java/com/fernando/connected_minds_api/controllers/InviteController.java
+++ src/main/java/com/fernando/connected_minds_api/controllers/InviteController.java
@@ -2,6 +2,7 @@ package com.fernando.connected_minds_api.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,10 @@ import com.fernando.connected_minds_api.responses.InviteResponse;
 import com.fernando.connected_minds_api.services.InviteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.fernando.connected_minds_api.models.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/invites")
@@ -21,4 +26,11 @@ public class InviteController {
     public ResponseEntity<InviteResponse> createInvite(@RequestBody @Valid InviteRequest inviteRequest) {
         return ResponseEntity.created(null).body(inviteService.createInvite(inviteRequest.fromID()));
     }
+
+    @GetMapping("/{inviteID}/accept")
+    public ResponseEntity<InviteResponse> acceptInvite(
+        @AuthenticationPrincipal User user,
+        @PathVariable UUID inviteID) {
+            return ResponseEntity.ok().body(inviteService.acceptInvite(user, inviteID));
+        }
 }
