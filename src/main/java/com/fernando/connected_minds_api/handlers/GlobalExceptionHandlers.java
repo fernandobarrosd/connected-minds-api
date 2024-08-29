78da281c712e9429ddbbe9d0145be57ec3082154
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-29 11:48:24 -0300

    Create handleUserIsAlreadyExiststInCommunityOrGroup and handleCannotLikeTheOwnPost exception handlers

--- src/main/java/com/fernando/connected_minds_api/handlers/GlobalExceptionHandlers.java
+++ src/main/java/com/fernando/connected_minds_api/handlers/GlobalExceptionHandlers.java
@@ -1,8 +1,10 @@
 package com.fernando.connected_minds_api.handlers;
 
+import com.fernando.connected_minds_api.exceptions.CannotLikeTheOwnPostException;
 import com.fernando.connected_minds_api.exceptions.EntityAlreadyExistsException;
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
 import com.fernando.connected_minds_api.exceptions.JWTTokenInvalidException;
+import com.fernando.connected_minds_api.exceptions.UserIsAlreadyExiststInCommunityOrGroupException;
 import com.fernando.connected_minds_api.exceptions.UserIsNotOwnerOfResourceException;
 import com.fernando.connected_minds_api.responses.error.ErrorResponse;
 import com.fernando.connected_minds_api.responses.error.ErrorResponseWithFields;
@@ -65,6 +67,38 @@ public class GlobalExceptionHandlers {
         return ResponseEntity.status(statusCode).body(errorResponse);
     }
 
+    @ExceptionHandler(CannotLikeTheOwnPostException.class)
+    public ResponseEntity<ErrorResponse> handleCannotLikeTheOwnPost(
+            CannotLikeTheOwnPostException exception,
+            HttpServletRequest request) {
+
+        int statusCode = HttpStatus.FORBIDDEN.value();
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(exception.getMessage())
+                .statusCode(statusCode)
+                .path(request.getRequestURI())
+                .date(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    @ExceptionHandler(UserIsAlreadyExiststInCommunityOrGroupException.class)
+    public ResponseEntity<ErrorResponse> handleUserIsAlreadyExiststInCommunityOrGroup(
+        UserIsAlreadyExiststInCommunityOrGroupException exception,
+        HttpServletRequest request) {
+
+                int statusCode = HttpStatus.BAD_REQUEST.value();
+
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(exception.getMessage())
+                .statusCode(statusCode)
+                .path(request.getRequestURI())
+                .date(LocalDateTime.now())
+                .build();
+        return ResponseEntity.badRequest().body(errorResponse);
+        }
+
+    
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEntityAlreadyExists(
             EntityAlreadyExistsException exception,
