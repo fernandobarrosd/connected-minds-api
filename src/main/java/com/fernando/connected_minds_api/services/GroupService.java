8044e70d1a255913c86a207854f1402eadeba396
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-30 16:13:40 -0300

    Update addMemberOnGroup method on GroupService

--- src/main/java/com/fernando/connected_minds_api/services/GroupService.java
+++ src/main/java/com/fernando/connected_minds_api/services/GroupService.java
@@ -1,8 +1,10 @@
 package com.fernando.connected_minds_api.services;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
+import com.fernando.connected_minds_api.exceptions.UserIsAlreadyExiststInCommunityOrGroupException;
 import com.fernando.connected_minds_api.models.Group;
 import com.fernando.connected_minds_api.models.User;
 import com.fernando.connected_minds_api.repositories.GroupRepository;
@@ -16,6 +18,16 @@ public class GroupService {
     public void addMemberOnGroup(UUID groupID, User member) {
         Group group = groupRepository.findById(groupID)
                 .orElseThrow(() -> new EntityNotFoundException("Group is not exists"));
+        
+        Optional<User> user = group
+                .getMembers()
+                .stream().
+                filter(userMember -> userMember.getId() == member.getId())
+                .findFirst();
+
+        if (user.isPresent()) {
+                throw new UserIsAlreadyExiststInCommunityOrGroupException("User is already exists in group#%d".formatted(groupID));               
+        }
         
         group.getMembers().add(member);
 
