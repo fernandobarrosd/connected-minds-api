f825b7bc43b7f6c5d964803360632216365fb804
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-08-30 16:14:14 -0300

    Remove likes attribute on UpdatePostRequest

--- src/main/java/com/fernando/connected_minds_api/requests/UpdatePostRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/UpdatePostRequest.java
@@ -1,14 +1,10 @@
 package com.fernando.connected_minds_api.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PositiveOrZero;
 
 public record UpdatePostRequest(
         @NotBlank(message = "content field not should be empty")
         String content,
 
         @NotBlank(message = "photoURL field not should be empty")
-        String photoURL,
-
-        @PositiveOrZero(message = "likes field should be positive or zero")
-        Long likes) {}
+        String photoURL) {}
