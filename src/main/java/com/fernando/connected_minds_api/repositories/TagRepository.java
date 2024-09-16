d9919e5f0e76733defe2e1e52eac6046119511c6
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-09-16 20:55:30 -0300

    Define findByName method on TagRepository jpa interface

--- src/main/java/com/fernando/connected_minds_api/repositories/TagRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/TagRepository.java
@@ -2,7 +2,9 @@ package com.fernando.connected_minds_api.repositories;
 
 import com.fernando.connected_minds_api.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+    Optional<Tag> findByName(String name);
 }
