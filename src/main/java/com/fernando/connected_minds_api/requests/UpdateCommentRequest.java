8967c79085532f6b563657f862adf95c1e653977
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-09-16 20:57:54 -0300

    Remove Long likes field on UpdateCommentRequest

--- src/main/java/com/fernando/connected_minds_api/requests/UpdateCommentRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/UpdateCommentRequest.java
@@ -1,12 +1,7 @@
 package com.fernando.connected_minds_api.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
 
 public record UpdateCommentRequest(
         @NotBlank(message = "content field not should be empty")
-        String content,
-
-        @Positive(message = "likes field should be positive or zero")
-        Long likes) {
-}
+        String content) {}
