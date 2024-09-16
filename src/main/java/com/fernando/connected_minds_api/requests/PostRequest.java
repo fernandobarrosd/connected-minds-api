7f9bf198677095fe5bec67e76719061bd9dcf251
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-09-16 20:56:10 -0300

    Add PostLocationType enum attribute on PostRequest

--- src/main/java/com/fernando/connected_minds_api/requests/PostRequest.java
+++ src/main/java/com/fernando/connected_minds_api/requests/PostRequest.java
@@ -2,6 +2,9 @@ package com.fernando.connected_minds_api.requests;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import com.fernando.connected_minds_api.enums.PostLocationType;
+import com.fernando.connected_minds_api.annotations.EnumValidator;
+
 
 public record PostRequest(
         @NotNull(message = "content field is required")
@@ -11,4 +14,8 @@ public record PostRequest(
         String photoURL,
         
         @NotNull(message = "locationID field is required")
-        String locationID) {}
+        String locationID,
+        
+        @NotNull(message = "content field is required")
+        @EnumValidator(enumValues = {"COMMUNITY", "GROUP"},  message = "locationType field should be COMMUNITY or GROUP")
+        PostLocationType locationType) {}
