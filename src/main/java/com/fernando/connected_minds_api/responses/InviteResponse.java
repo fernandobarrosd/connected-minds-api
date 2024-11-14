a29a3cb1c07dc041e3be63165d3f2f8925e14330
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-11-14 17:24:12 -0300

    Add InviteType enum type attribute in InviteResponse record

--- src/main/java/com/fernando/connected_minds_api/responses/InviteResponse.java
+++ src/main/java/com/fernando/connected_minds_api/responses/InviteResponse.java
@@ -1,18 +1,22 @@
 package com.fernando.connected_minds_api.responses;
 
 import java.util.UUID;
+
+import com.fernando.connected_minds_api.enums.InviteType;
 import com.fernando.connected_minds_api.models.Invite;
 import lombok.Builder;
 
 @Builder
 public record InviteResponse(
     UUID id,
-    UUID fromID) {
+    UUID fromID,
+    InviteType inviteType) {
 
         public static InviteResponse toResponse(Invite invite) {
             return InviteResponse.builder()
                 .id(invite.getId())
                 .fromID(invite.getFromID())
+                .inviteType(invite.getType())
                 .build();
         }
     }
