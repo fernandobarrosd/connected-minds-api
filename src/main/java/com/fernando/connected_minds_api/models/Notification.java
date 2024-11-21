56dd8319e62614ec6a4d494be9e52e6e02c21875
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-11-20 21:12:43 -0300

    Rename table name to notification_table and update Notification entity class

--- src/main/java/com/fernando/connected_minds_api/models/Notification.java
+++ src/main/java/com/fernando/connected_minds_api/models/Notification.java
@@ -1,9 +1,13 @@
 package com.fernando.connected_minds_api.models;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+import com.fernando.connected_minds_api.enums.NotificationType;
 import com.fernando.connected_minds_api.models.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,11 +17,15 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "notification_table")
 public class Notification extends BaseEntity {
+    @Column(name = "from_id", nullable = false)
+    private UUID fromID;
+
     @Column(nullable = false)
     private String content;
 
-    @Column(name = "photo_url", nullable = false)
+    @Column(name = "photo_url")
     private String photoURL;
 
     @Column(name = "is_viewed", nullable = false)
@@ -26,11 +34,19 @@ public class Notification extends BaseEntity {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    public Notification(String content, String photoURL, Boolean isViewed) {
+    @Column(nullable = false)
+    private NotificationType type;
+
+    @ManyToOne
+    private User owner;
+
+    public Notification(String content, String photoURL, NotificationType type, UUID fromID) {
         super(null);
         this.content = content;
         this.photoURL = photoURL;
         this.isViewed = false;
         this.createdAt = LocalDateTime.now();
+        this.type = type;
+        this.fromID = fromID;
     }
 }
