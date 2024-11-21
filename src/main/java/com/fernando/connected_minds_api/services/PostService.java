8d3b6e2127c06e5ec7c50832df51aaf5f2a321e5
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-11-20 21:13:47 -0300

    Update PostService service class

--- src/main/java/com/fernando/connected_minds_api/services/PostService.java
+++ src/main/java/com/fernando/connected_minds_api/services/PostService.java
@@ -3,42 +3,59 @@ package com.fernando.connected_minds_api.services;
 import com.fernando.connected_minds_api.exceptions.CannotLikeTheOwnPostException;
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
 import com.fernando.connected_minds_api.exceptions.UserIsNotOwnerOfResourceException;
-import com.fernando.connected_minds_api.models.Comment;
 import com.fernando.connected_minds_api.models.LikePost;
 import com.fernando.connected_minds_api.models.Post;
 import com.fernando.connected_minds_api.models.User;
-import com.fernando.connected_minds_api.repositories.CommentRepository;
-import com.fernando.connected_minds_api.repositories.CommunityRepository;
+import com.fernando.connected_minds_api.models.Community;
+import com.fernando.connected_minds_api.models.Group;
 import com.fernando.connected_minds_api.repositories.PostRepository;
 import com.fernando.connected_minds_api.requests.CommentRequest;
+import com.fernando.connected_minds_api.requests.FindAllPostsRequest;
+import com.fernando.connected_minds_api.requests.NotificationRequest;
 import com.fernando.connected_minds_api.requests.PostRequest;
 import com.fernando.connected_minds_api.requests.UpdatePostRequest;
 import com.fernando.connected_minds_api.requests.params.PaginationQueryParams;
 import com.fernando.connected_minds_api.responses.CommentResponse;
+import com.fernando.connected_minds_api.responses.NotificationResponse;
 import com.fernando.connected_minds_api.responses.PostResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
+import com.fernando.connected_minds_api.enums.NotificationType;
+import com.fernando.connected_minds_api.enums.PostLocationType;
+
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
-    private final CommunityRepository communityRepository;
+    private final CommentService commentService;
+    private final CommunityService communityService;
+    private final GroupService groupService;
     private final LikePostService likePostService;
+    private final NotificationService notificationService;
 
     public PostResponse createPost(PostRequest postRequest, User owner) {
-        UUID locationID = UUID.fromString(postRequest.locationID());
-
-        if (!communityRepository.existsById(locationID)) {
-            throw new EntityNotFoundException("Community or group is not exists");
+        UUID locationID = postRequest.locationID();
+        NotificationType notificationType = NotificationType.POST;
+        Map<String, Object> data = new HashMap<>();
+        
+        
+        if (postRequest.locationType() == PostLocationType.COMMUNITY) {
+            Community community = communityService.findCommunityByID(locationID);
+            data.put("name", community.getName());
+            data.put("members", community.getMembers());
         }
+        else {
+            Group group = groupService.findGroupByID(locationID);
+            data.put("name", group.getName());
+            data.put("members", group.getMembers());
+        }
+        
         Post post = new Post(
                 postRequest.content(),
                 postRequest.photoURL(),
@@ -47,22 +64,39 @@ public class PostService {
         );
         postRepository.save(post);
 
+        String content = "%s postou no %s".formatted(owner.getUsername(), data.get("name"));
+        List<User> members = (List<User>) data.get("members");
+        
+        NotificationRequest request = new NotificationRequest(
+            post.getId(),
+            content,
+            null,
+            notificationType
+        );
+        NotificationResponse notificationResponse = notificationService.saveNotification(request, owner);
+
+
+        members.forEach(member -> {
+            notificationService.sendNotification(member.getId(), notificationResponse);
+        });
+        
         return PostResponse.toResponse(post);
     }
 
-    public List<PostResponse> findAllPosts(String locationID) {
-        UUID locationIdUUID = UUID.fromString(locationID);
+    public List<PostResponse> findAllPosts(FindAllPostsRequest request) {
+        UUID locationID = request.locationID();
 
-        if (!communityRepository.existsById(locationIdUUID)) {
-            throw new EntityNotFoundException("Community or group is not exists");
+        if (request.locationType() == PostLocationType.COMMUNITY) {
+            communityService.existsCommunityById(locationID);
+        }
+        else {
+            groupService.existsGroupById(locationID);
         }
 
-        return postRepository.findAllByLocationID(locationIdUUID)
+        return postRepository.findAllByLocationID(locationID)
             .stream()
             .map(PostResponse::toResponse)
             .toList();
-
-
     }
 
     public PostResponse findPostByID(UUID postID) {
@@ -78,15 +112,13 @@ public class PostService {
         Post post = postRepository.findById(postID)
                 .orElseThrow(() -> new EntityNotFoundException("Post is not exists"));
 
-        Comment comment = new Comment(commentRequest.content(), user, post);
+        CommentResponse commentResponse = commentService.saveComment(commentRequest.content(), user, post);
 
-        commentRepository.save(comment);
-
-        return CommentResponse.toResponse(comment);
+        return commentResponse;
     }
 
     public void deletePost(UUID postID, UUID userID) {
-        Post post = postRepository.findById(userID)
+        Post post = postRepository.findById(postID)
             .orElseThrow(() -> new EntityNotFoundException("Post is not exists"));
         
         if (post.getOwner().getId() != userID) {
@@ -119,23 +151,18 @@ public class PostService {
         if (!postRepository.existsById(postID)) {
             throw new EntityNotFoundException("Post is not exists");
         }
-        Pageable pageable = PageRequest.of(pagination.getPage(), pagination.getItemsPerPage(), Sort.by("likes").descending());
-
-        return commentRepository.findAllByPostId(postID, pageable)
-                .stream()
-                .map(CommentResponse::toResponse)
-                .toList();
+        return commentService.findAllComments(pagination, postID);
     }
 
     public LikePost createLike(User user, UUID postID) {
         Post post = postRepository.findById(postID)
                 .orElseThrow(() -> new EntityNotFoundException("Post is not exists"));
 
-        LikePost like = new LikePost(user, post);
-
         if (post.getOwner().getId() == user.getId()) {
             throw new CannotLikeTheOwnPostException("Actually auth user dont like the own post");
         }
+
+        LikePost like = new LikePost(user, post);
 
         likePostService.saveLike(like);
 
