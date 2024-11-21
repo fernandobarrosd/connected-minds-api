26367090d163b466653a4246a49a620d8556733a
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-11-20 21:00:12 -0300

    Update WebSocketConfig configuration class

--- src/main/java/com/fernando/connected_minds_api/config/WebSocketConfig.java
+++ src/main/java/com/fernando/connected_minds_api/config/WebSocketConfig.java
@@ -11,14 +11,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topics");
+        registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
     }
+    
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
             .addEndpoint("/ws")
-            .setAllowedOrigins("*")
+            .setAllowedOriginPatterns("*")
             .withSockJS();
+            registry
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*");
     }
 }
