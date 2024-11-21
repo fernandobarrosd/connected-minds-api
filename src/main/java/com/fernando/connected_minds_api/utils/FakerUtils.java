49c803500de49b8b51b3c5e976956768e21b71b7
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-11-20 21:10:21 -0300

    Add generateFakeLoginRequest and generateFakeRegisterRequest static methods in FakerUtils utility class

--- src/main/java/com/fernando/connected_minds_api/utils/FakerUtils.java
+++ src/main/java/com/fernando/connected_minds_api/utils/FakerUtils.java
@@ -6,9 +6,10 @@ import com.fernando.connected_minds_api.enums.PostLocationType;
 import com.fernando.connected_minds_api.enums.UserGenre;
 import com.fernando.connected_minds_api.models.Post;
 import com.fernando.connected_minds_api.models.User;
+import com.fernando.connected_minds_api.requests.LoginRequest;
 import com.fernando.connected_minds_api.requests.PostRequest;
+import com.fernando.connected_minds_api.requests.RegisterRequest;
 import net.datafaker.Faker;
-
 
 public final class FakerUtils {
     private FakerUtils() {
@@ -55,6 +56,31 @@ public final class FakerUtils {
             null,
             UUID.randomUUID(),
             owner
+        );
+    }
+
+    public static LoginRequest generateFakeLoginRequest(Faker faker) {
+        String ownerEmail = faker.internet().emailAddress();
+        String ownerPassword = faker.internet().password(8, 8);
+
+        return new LoginRequest(ownerEmail, ownerPassword);
+    }
+
+    public static RegisterRequest generateFakeRegisterRequest(Faker faker) {
+        LocalDate birthDate = LocalDate.of(1980, 10, 5);
+        String username = faker.regexify("username_[A-Za-z]{4,8}");
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password(8, 8);
+        String genre = faker.regexify("MALE|FEMALE");
+
+        return new RegisterRequest(
+            username, 
+            email,
+            password,
+    null, 
+  null, 
+            genre, 
+            birthDate.toString()
         );
     }
 }
