2246c033eb1c1ce5f811bbdd5ee1474be05234ad
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-07 14:56:31 -0300

    Create NotificationPayload class

---
+++ src/main/java/com/fernando/connected_minds_api/payloads/NotificationPayload.java
@@ -0,0 +1,28 @@
+package com.fernando.connected_minds_api.payloads;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fernando.connected_minds_api.enums.NotificationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = PostNotificationPayload.class, name = "POST")
+})
+public class NotificationPayload {
+    private NotificationType type;
+
+}
