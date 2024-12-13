9434535d763af088e69669a86ee7338f62801973
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-13 13:01:45 -0300

    Add CommandLineRunner bean, AuthService dependency in ConnectedMindsApplication main class

--- src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
+++ src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
@@ -3,11 +3,38 @@ package com.fernando.connected_minds_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.CommandLineRunner;
+import com.fernando.connected_minds_api.services.AuthService;
+import com.fernando.connected_minds_api.requests.RegisterRequest;
+import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+
 
 @SpringBootApplication
 @EnableCaching
+@RequiredArgsConstructor
 public class ConnectedMindsApiApplication {
+    private final AuthService authService;
+
     public static void main(String[] args) {
         SpringApplication.run(ConnectedMindsApiApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            RegisterRequest user = new RegisterRequest(
+                "fernandobarrosd",
+                "fbarros@test.com",
+                "fbarros123",
+                null,
+                null,
+                "MALE",
+                "2004-10-09"
+            );
+
+            authService.registerUser(user);
+        };
+    }
+
 }
