5d7377ca995cbbee3ba0c074889149e4d4360f01
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-13 13:02:23 -0300

    Add username, photoURL, bannerURL and bio attributes in AuthResponse record

--- src/main/java/com/fernando/connected_minds_api/responses/AuthResponse.java
+++ src/main/java/com/fernando/connected_minds_api/responses/AuthResponse.java
@@ -1,3 +1,10 @@
 package com.fernando.connected_minds_api.responses;
 
-public record AuthResponse(String token, String refreshToken, String tokenExpiresAt) {}
+public record AuthResponse(
+    String token, 
+    String refreshToken, 
+    String tokenExpiresAt,
+    String username,
+    String photoURL,
+    String bannerURL,
+    String bio) {}
