41d16053c1da30800639dc6f8c3621980895dd9e
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-17 19:16:06 -0300

    Create FindAllPostsQueryParams query params class

---
+++ src/main/java/com/fernando/connected_minds_api/requests/params/FindAllPostsQueryParams.java
@@ -0,0 +1,21 @@
+package com.fernando.connected_minds_api.requests.params;
+
+import java.util.UUID;
+import com.fernando.connected_minds_api.annotations.EnumValidator;
+import com.fernando.connected_minds_api.enums.PostLocationType;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class FindAllPostsQueryParams extends PaginationQueryParams {
+    @NotNull(message = "locationID field is required")
+    UUID locationID;
+
+    @NotNull(message = "content field is required")
+    @EnumValidator(enumValues = {"COMMUNITY", "GROUP"},  message = "locationType field should be COMMUNITY or GROUP")
+    PostLocationType locationType;
+    
+}
