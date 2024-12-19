c3899c054a77fc25322e7643116a6d212158162d
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-19 12:55:32 -0300

    Update findAllByLocationID method return type to Page<Post> in PostRespository

--- src/main/java/com/fernando/connected_minds_api/repositories/PostRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/PostRepository.java
@@ -1,11 +1,12 @@
 package com.fernando.connected_minds_api.repositories;
 
 import com.fernando.connected_minds_api.models.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 import java.util.UUID;
 
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
-    List<Post> findAllByLocationID(UUID locationID);
+    Page<Post> findAllByLocationID(UUID locationID, Pageable pageable);
 }
