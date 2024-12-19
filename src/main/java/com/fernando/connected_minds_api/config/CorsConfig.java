59baed54972e2b78b027a0d5331c94eb3859c0f9
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-19 12:47:57 -0300

    Add orignURL attribute and update addCorsMappings method in CorsConfig config class

--- src/main/java/com/fernando/connected_minds_api/config/CorsConfig.java
+++ src/main/java/com/fernando/connected_minds_api/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.fernando.connected_minds_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+	@Value("${spring.security.orign-url}")
+	private String orignURL;
+
     @Bean
     WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins(orignURL);
 			}
 		};
 	}   
