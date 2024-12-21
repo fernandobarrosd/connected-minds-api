151d0287dc07c9aefb2289eb444be095fd232028
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 17:55:17 -0300

    Update ConnectedMindsApiApplication main class

--- src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
+++ src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
@@ -2,16 +2,16 @@ package com.fernando.connected_minds_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.CommandLineRunner;
 import com.fernando.connected_minds_api.services.AuthService;
+import com.fernando.connected_minds_api.utils.JSONReader;
 import com.fernando.connected_minds_api.requests.RegisterRequest;
 import org.springframework.context.annotation.Bean;
 import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
 
 
 @SpringBootApplication
-@EnableCaching
 @RequiredArgsConstructor
 public class ConnectedMindsApiApplication {
     private final AuthService authService;
@@ -23,17 +23,10 @@ public class ConnectedMindsApiApplication {
     @Bean
     CommandLineRunner commandLineRunner() {
         return args -> {
-            RegisterRequest user = new RegisterRequest(
-                "fernandobarrosd",
-                "fbarros@test.com",
-                "fbarros123",
-                null,
-                null,
-                "MALE",
-                "2004-10-09"
-            );
+            RegisterRequest[] users = JSONReader.readJSON(RegisterRequest[].class, "users.json");
 
-            authService.registerUser(user);
+            Arrays.stream(users)
+            .forEach(authService::registerUser);  
         };
     }
 
