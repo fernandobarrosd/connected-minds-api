c198cf89e34a02ad242889d92ce1c8602c7aa014
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 18:55:13 -0300

    Remove PostLocationIDQueryParam query param class

--- src/main/java/com/fernando/connected_minds_api/requests/params/PostLocationIDQueryParam.java
+++
@@ -1,7 +0,0 @@
-package com.fernando.connected_minds_api.requests.params;
-
-import jakarta.validation.constraints.NotNull;
-
-public record PostLocationIDQueryParam(
-    @NotNull(message = "locationID query param is required")
-    String locationID) {}
