6ea84a3781f839339597dadf4809493945d9599b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 19:08:49 -0300

    Update findAllByPostId and findAllCommentsOfComment methods return type to Page<Comment>

--- src/main/java/com/fernando/connected_minds_api/repositories/CommentRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/CommentRepository.java
@@ -1,16 +1,16 @@
 package com.fernando.connected_minds_api.repositories;
 
 import com.fernando.connected_minds_api.models.Comment;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
     @Query("SELECT c FROM Comment c WHERE c.post.id = :postID AND c.comment IS null")
-    List<Comment> findAllByPostId(UUID postID, Pageable pageable);
+    Page<Comment> findAllByPostId(UUID postID, Pageable pageable);
 
     @Query("SELECT c.comments FROM Comment c WHERE c.id = :commentID")
-    List<Comment> findAllCommentsOfComment(UUID commentID, Pageable pageable);
+    Page<Comment> findAllCommentsOfComment(UUID commentID, Pageable pageable);
 }
