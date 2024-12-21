65aa6028b92bb5e2181693a0efa2c41539d26f67
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 19:02:21 -0300

    Update findAllCommunities method return type to Page<Community> in UserRepository repository interface

--- src/main/java/com/fernando/connected_minds_api/repositories/UserRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 
     @Query("SELECT u.communities FROM User u WHERE u.id = :userID")
-    List<Community> findAllCommunities(UUID userID, Pageable pageable);
+    Page<Community> findAllCommunities(UUID userID, Pageable pageable);
     
     Page<User> findAllByUsernameContaining(String username, Pageable pageable);
 }
