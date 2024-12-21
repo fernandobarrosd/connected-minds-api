d13e86ea7a4ffadb60318723f095f8cabbe566b2
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 19:00:23 -0300

    Add Valid annotation in SearchQueryParams searchQueryParam search method param and add SearchControllerDocumentation implementation in SearchController controller class

--- src/main/java/com/fernando/connected_minds_api/controllers/SearchController.java
+++ src/main/java/com/fernando/connected_minds_api/controllers/SearchController.java
@@ -4,20 +4,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.fernando.connected_minds_api.queryparams.SearchQueryParams;
-import com.fernando.connected_minds_api.responses.PaginationResponse;
+import com.fernando.connected_minds_api.responses.pagination.PaginationResponse;
 import com.fernando.connected_minds_api.services.SearchService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import com.fernando.connected_minds_api.docs.SearchControllerDocumentation;
+
 
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
-public class SearchController {
+public class SearchController implements SearchControllerDocumentation {
     private final SearchService searchService;
 
     @GetMapping
-    public ResponseEntity<PaginationResponse<?>> search(SearchQueryParams searchQueryParam) {
+    public ResponseEntity<PaginationResponse<?>> search(@Valid SearchQueryParams searchQueryParam) {
         return ResponseEntity.ok(searchService.search(searchQueryParam));
     }
 }
