3b7182487b4b44fa5098bb64cd5ed05edc87d0d1
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 18:55:46 -0300

    Add findAllByNameContaining method in CommunityRepository repository class

--- src/main/java/com/fernando/connected_minds_api/repositories/CommunityRepository.java
+++ src/main/java/com/fernando/connected_minds_api/repositories/CommunityRepository.java
@@ -3,6 +3,10 @@ package com.fernando.connected_minds_api.repositories;
 import com.fernando.connected_minds_api.models.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
 
 public interface CommunityRepository extends JpaRepository<Community, UUID> {
+    Page<Community> findAllByNameContaining(String name, Pageable pageable);
 }
