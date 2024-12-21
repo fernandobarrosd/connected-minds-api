f2ccf3002a79dabe67549be59d4114340640d86e
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-21 19:06:12 -0300

    Remove isOnline parameter in constructor, remove isOnline variable in toResponse method on UserResponse

--- src/main/java/com/fernando/connected_minds_api/responses/UserResponse.java
+++ src/main/java/com/fernando/connected_minds_api/responses/UserResponse.java
@@ -1,6 +1,5 @@
 package com.fernando.connected_minds_api.responses;
 
-import com.fernando.connected_minds_api.enums.UserStatus;
 import com.fernando.connected_minds_api.models.User;
 import java.util.UUID;
 
@@ -8,18 +7,14 @@ public record UserResponse(
         UUID id,
         String username,
         String genre,
-        Boolean isOnline,
         String bio,
         String photoURL,
         String bannerURL) {
     public static UserResponse toResponse(User user) {
-        Boolean isOnline = user.getStatus() == UserStatus.ONLINE;
-
         return new UserResponse(
                 user.getId(),
                 user.getUsername(),
                 user.getGenre().getValue(),
-                isOnline,
                 user.getBio(),
                 user.getPhotoURL(),
                 user.getBannerURL()
