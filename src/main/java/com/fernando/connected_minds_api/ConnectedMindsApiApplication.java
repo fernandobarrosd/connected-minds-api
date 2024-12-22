9d3b084c29889155d2f138e29001869e63a6bebc
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-22 19:11:13 -0300

    Update ConnectedMindsApiApplication main class

--- src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
+++ src/main/java/com/fernando/connected_minds_api/ConnectedMindsApiApplication.java
@@ -2,32 +2,11 @@ package com.fernando.connected_minds_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import com.fernando.connected_minds_api.services.AuthService;
-import com.fernando.connected_minds_api.utils.JSONReader;
-import com.fernando.connected_minds_api.requests.RegisterRequest;
-import org.springframework.context.annotation.Bean;
-import lombok.RequiredArgsConstructor;
-import java.util.Arrays;
 
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class ConnectedMindsApiApplication {
-    private final AuthService authService;
-
     public static void main(String[] args) {
         SpringApplication.run(ConnectedMindsApiApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return args -> {
-            RegisterRequest[] users = JSONReader.readJSON(RegisterRequest[].class, "users.json");
-
-            Arrays.stream(users)
-            .forEach(authService::registerUser);  
-        };
-    }
-
 }
