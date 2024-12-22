f830be89f3e67ddecbc09ab8bc2587abf890bd3d
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-12-22 19:14:44 -0300

    Add CacheManager dependency and update UserService service class

--- src/main/java/com/fernando/connected_minds_api/services/UserService.java
+++ src/main/java/com/fernando/connected_minds_api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.fernando.connected_minds_api.services;
 
 import com.fernando.connected_minds_api.exceptions.EntityNotFoundException;
+import com.fernando.connected_minds_api.managers.CacheManager;
 import com.fernando.connected_minds_api.models.Community;
 import com.fernando.connected_minds_api.models.User;
 import com.fernando.connected_minds_api.queryparams.PaginationQueryParams;
@@ -8,7 +9,6 @@ import com.fernando.connected_minds_api.repositories.UserRepository;
 import com.fernando.connected_minds_api.responses.CommunityResponse;
 import com.fernando.connected_minds_api.responses.UserResponse;
 import com.fernando.connected_minds_api.responses.pagination.PaginationResponse;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,16 +21,30 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final CacheManager cacheManager;
 
     public UserResponse findUserByUsername(String username) {
+        String userCacheKey = "USER_%s".formatted(username);
+
+        User userCache = cacheManager.getCacheValue(userCacheKey);
+
+        if (userCache != null) {
+                return UserResponse.toResponse(userCache);
+        }
+
+
         User user = userRepository
                 .findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("%s is not exists".formatted(username)));
+        
+        
+        cacheManager.addCacheValue(userCacheKey, user);
 
         return UserResponse.toResponse(user);
     }
 
-    public PaginationResponse<CommunityResponse> findAllUserCommunities(UUID userID, PaginationQueryParams queryParams) {
+    public PaginationResponse<CommunityResponse> findAllUserCommunities(UUID userID,
+            PaginationQueryParams queryParams) {
         Integer itemsPerPage = queryParams.getItemsPerPage();
         Integer page = queryParams.getPage();
 
@@ -39,40 +53,60 @@ public class UserService {
         Page<Community> communityPage = userRepository.findAllCommunities(userID, pageable);
 
         List<CommunityResponse> communities = communityPage
-        .stream()
-        .map(CommunityResponse::toResponse)
-        .toList();
+                .stream()
+                .map(CommunityResponse::toResponse)
+                .toList();
 
         return PaginationResponse.toResponse(
-            communityPage.hasNext(),
-            communityPage.getTotalPages(),
-            communityPage.getTotalElements(),
-            itemsPerPage,
-            page,
-            communities
-        );
+                communityPage.hasNext(),
+                communityPage.getTotalPages(),
+                communityPage.getTotalElements(),
+                itemsPerPage,
+                page,
+                communities);
     }
 
     public User findUserById(UUID userID) {
         return userRepository.findById(userID)
-        .orElseThrow(() -> new EntityNotFoundException("User is not exists"));
+                .orElseThrow(() -> new EntityNotFoundException("User is not exists"));
     }
 
     public PaginationResponse<UserResponse> searchUser(String search, Integer page, Integer itemsPerPage) {
+        String cacheUsersKey = "SEARCHED_USERS_%s_PAGE_%d".formatted(
+            search.replaceAll(" ", "_"),
+            page
+        );
+
+        Page<User> userPageCache = cacheManager.getCacheValue(cacheUsersKey);
+
+        if (userPageCache != null) {
+            List<UserResponse> users = userPageCache.get()
+                    .map(UserResponse::toResponse)
+                    .toList();
+
+            return PaginationResponse.toResponse(
+                    userPageCache.hasNext(),
+                    userPageCache.getTotalPages(),
+                    userPageCache.getTotalElements(),
+                    itemsPerPage,
+                    page,
+                    users);
+        }
         Pageable pageable = PageRequest.of(page, itemsPerPage);
         Page<User> userPage = userRepository.findAllByUsernameContaining(search, pageable);
 
+        cacheManager.addCacheValue(cacheUsersKey, userPage);
+
         List<UserResponse> users = userPage.get()
-        .map(UserResponse::toResponse)
-        .toList();
+                .map(UserResponse::toResponse)
+                .toList();
 
         return PaginationResponse.toResponse(
-            userPage.hasNext(),
-            userPage.getTotalPages(),
-            userPage.getTotalElements(),
-            itemsPerPage,
-            page,
-            users
-        );
+                userPage.hasNext(),
+                userPage.getTotalPages(),
+                userPage.getTotalElements(),
+                itemsPerPage,
+                page,
+                users);
     }
 }
