59a2258cc46defdedf6f8c9750a433552bebf5b4
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-02-14 15:58:13 -0300

    Create WebMvcConfig

---
+++ src/main/java/com/fernando/connected_minds_api/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.fernando.connected_minds_api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+    @Value("${spring.security.orign-url}")
+	private String orignURL;
+
+    @Bean
+    WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(orignURL);
+			}
+		};
+	}
+
+}
