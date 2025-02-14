a6c0b569cd811735742c2907ed2ba51716759a7a
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-02-14 15:01:28 -0300

    Create UserFactory

---
+++ src/main/java/com/fernando/connected_minds_api/user/UserFactory.java
@@ -0,0 +1,37 @@
+package com.fernando.connected_minds_api.user;
+
+import java.util.UUID;
+
+import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import com.fernando.connected_minds_api.formatters.DateTimeFormatters;
+
+public final class UserFactory {
+    private UserFactory() throws Exception {
+        throw new Exception("UserFactory must not be instanced");
+    }
+
+    public static User createUser(Faker faker) {
+        LocalDate thirtyYearsOldBirthDate = LocalDate.parse("09-10-2012", DateTimeFormatters.LOCAL_DATE_FORMATTER);
+
+        UUID userID = UUID.randomUUID();
+        String username = faker.name().username().replace(".", "_");
+        String email = faker.internet().safeEmailAddress();
+        String password = faker.internet().password(6, 7, true, false);
+        String photoURL = faker.internet().image();
+        String bannerURL = faker.internet().image();
+        String bio = faker.lorem().characters();
+
+        return User.builder()
+            .id(userID)
+            .username(username)
+            .email(email)
+            .password(password)
+            .photoURL(photoURL)
+            .bannerURL(bannerURL)
+            .bio(bio)
+            .birthDate(thirtyYearsOldBirthDate)
+            .build();
+
+    }
+}
