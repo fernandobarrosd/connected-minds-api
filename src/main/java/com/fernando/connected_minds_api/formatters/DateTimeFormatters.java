1b12cc0867071709d61d89fb1d82611a313b73c0
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-02-14 15:01:00 -0300

    Create DateTimeFormatters

---
+++ src/main/java/com/fernando/connected_minds_api/formatters/DateTimeFormatters.java
@@ -0,0 +1,8 @@
+package com.fernando.connected_minds_api.formatters;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatters {
+    public static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+}
