388012456fb6f804d35c91e99a65c42161488d3a
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-02-22 16:42:53 -0300

    Remove bannerURL attribute and add userGenre attribute in AuthResponse

--- src/main/java/com/fernando/connected_minds_api/auth/AuthResponse.java
+++ src/main/java/com/fernando/connected_minds_api/auth/AuthResponse.java
@@ -1,15 +1,17 @@
 package com.fernando.connected_minds_api.auth;
 
 import java.util.UUID;
+
+import com.fernando.connected_minds_api.user.UserGenre;
 import lombok.Builder;
 
 @Builder
 public record AuthResponse(
-    String token, 
-    String refreshToken, 
-    String tokenExpiresAt,
-    UUID userId,
-    String username,
-    String photoURL,
-    String bannerURL,
-    String bio) {}
+        String token,
+        String refreshToken,
+        String tokenExpiresAt,
+        UUID userId,
+        String username,
+        String photoURL,
+        String bio,
+        UserGenre userGenre) {}
